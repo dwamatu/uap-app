6e961f6f417a79fb9c1e03bc27bed2c82d171d4a
mtoto_dev <mtoto.mgonjwa@gmail.com>
2016-10-13 11:45:54 +0300

    domPdf initialization Document Ready

--- app/Http/Controllers/LossCalculationController.php
+++ app/Http/Controllers/LossCalculationController.php
@@ -49,6 +49,7 @@ class LossCalculationController extends Controller
                                 }
                                 td {
                                     font-size: 80%;
+                               
                                 }
                                 p {
                                     font-size: 60%;
@@ -58,235 +59,181 @@ class LossCalculationController extends Controller
                                     width: 100%;
                                     /*height: 100%;*/
                                 }
-                            table{
-                                empty-cells: show;
-                                width: 75%;
-                                font-size: x-small;
-                            }
-                            table td,
-                            table th {
-                                min-width: 2em;
+                                 .ze_head {
+                                    left:5%;
+                                    padding: 5px;
+                                    width:95%;
+                                    /*position:absolute;*/
+                                    margin: auto auto;
+                                }
+                                th {
+                                    text-align:left;
+                                }
+                                .ze_first_table {
+                                    border: 1px solid black;                        
+                                    width:95%;
+                                    /*position:absolute;*/
+                                }
+                                    table{
+                                        empty-cells: show;
+                                        width: 75%;
+                                        font-size: x-small;
+                                    }
+                                    table td,
+                                    table th {
+                                        min-width: 2em;
                                 min-height: 2em;
-                            }
-                            .my_footer{
-                            page-break-after: always;
+                                    }
+                                    .my_footer{
+                                    page-break-after: always;
+        
+                                }          
 
-                        }
-                            img{
-                            width: 200px;
-                            height: 250px;
-                            }
-                        
+                                div > img{
+                                   height:300px;
+                                    width:300px;
+                                }
+
                         </style>
                         ';
         $the_title = '<center><h2>LOSS ASSESSMENT NOTE</h2></center>';
-        $the_header ='<table>
+        $the_header ='<table class="ze_head" border=1 cellspacing=0 cellpadding="4" align="center">
+                            <tr style="background-color:#DFDFDF;">
+                                <th colspan="4">A. FARMER DETAILS</th>
+                            </tr>
 
     <tr>
         <td><b>Farmers Name</b></td>
-        <td colspan="2">'. ($assessment->farmer->farmer_name).'</td>
+        <td colspan="1">'. ($assessment->farmer->farmer_name).'</td>
         <td><b>ID no</b></td>
         <td>'. ($assessment->farmer->id_number).'</td>
 
     </tr>
     <tr>
         <td><b>Farmers Zone</b></td>
-        <td colspan="2">'. ($assessment->farmer->farmer_zone).'</td>
+        <td colspan="1">'. ($assessment->farmer->farmer_zone).'</td>
         <td><b>A/C no</b></td>
         <td>'. ($assessment->farmer->account_number).'</td>
 
     </tr>
     <tr>
         <td><b>Staff Name</b></td>
-        <td colspan="3">'. ($assessment->user->first_name).' '.($assessment->user->last_name).'</td>
+        <td colspan="1">'. ($assessment->user->first_name).' '.($assessment->user->last_name).'</td>
+         <td><b>Planting Dates</b></td>
 
-
-    </tr>
-    <tr>
-
-        <td><b>Planting Dates</b></td>
-
-        <td>_________________________</td>
-        <td><b>Inspection Dates</b></td>
-        <td colspan="2">'. date('d-M-Y',strtotime($assessment->inspection_date)).'</td>
-
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
         <td></td>
 
     </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-
-    </tr>
-    <tr>
-        <td><b>Inspection no.</b></td>
-        <td></td>
 
 
-    </tr>
-    <tr>
-        <td><b>Cause of Loss:</b></td>
-         <td></td>
-       
-
-
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2">
-            Uniform or Non-uniform losses
-        </td>
-        <td  style="outline: thin solid"></td>
-        <td  style="outline: thin solid"></td>
-        <td  style="outline: thin solid"></td>
-        <td  style="outline: thin solid"></td>
-      
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2">
-            Hectares
-        </td>
-
-        <td style="outline: thin solid"> </td>
-        <td style="outline: thin solid"> </td>
-         <td colspan="2" style="outline: thin solid"> '. ($assessment-> farm->size_with_tobacco).' hectares</td>
-
-
-    </tr>
-    <tr style="outline: thin solid">
-        <td style="outline: thin solid" colspan="6"><b>Leaf Damage(Damage on the surface area of the leaf quantified as % or given in cm<sup>2</sup>
-            in case of drought</b></td>
-
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2">Average useful surface area <b>(a)</b></td>
-
-     
-        <td style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-          <td colspan="2" style="outline: thin solid"> '. ($assessment-> average_useful_sa).'</td>
-
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2">Normal leaf surface area <b>(b)</b></td>
-
-       
-        <td  style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-         <td colspan="2" style="outline: thin solid">'. ($assessment-> normal_leaf_sa).'</td>
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="6"><b> Plant Damage(number of leaves damaged on the plant)</b></td>
-
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2">Average no. of leaves on the plant <b>(c)</b></td>
-       
-        <td  style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-     
-          <td colspan="2" style="outline: thin solid"> '. ($assessment->average_leaves_no).'</td>
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2"> Normal no. leaves on the plant <b>(d)</b></td>
-      
-        <td style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-        
-            <td colspan="2" style="outline: thin solid"> '. ($assessment->normal_leaves_no).'</td>
-    </tr>
-    <tr style="outline: thin solid">
-        <td style="outline: thin solid" colspan="6"><b> Land damage (number of plants damaged on the land parcel)</b></td>
-
-    </tr>
-    <tr style="outline: thin solid">
-        <td style="outline: thin solid" colspan="2">Average no. of plants on the land <b>(e)</b></td>
-       
-        <td style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-        
-           <td colspan="2"style="outline: thin solid"> '. ($assessment->average_plant_no).'</td>
-    </tr>
-    <tr style="outline: thin solid">
-    <td  style="outline: thin solid" colspan="2">Established no. of plants <b>(f)</b></td>
-       
-        <td  style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-      
-         <td colspan="2" style="outline: thin solid">'. ($assessment->established_plant_no).'</td>
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2"><b>Percentage Loss (%)</b></td>
-       
-        <td  style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-      
-        <td colspan="2" style="outline: thin solid"> '. ($assessment->percentage_loss).'%</td>
-    </tr>
-    <tr style="outline: thin solid">
-        <td  style="outline: thin solid" colspan="2"> <b>Leaf stage of the plant</b></td>
-       
-        <td  style="outline: thin solid"></td>
-        <td style="outline: thin solid"></td>
-       
-         <td colspan="2" style="outline: thin solid"> '. ($assessment->leaf_stage).'</td>
-    </tr>
-
-
-
-    <tr>
-        <td colspan="6"><b>Formulaes</b></td>
-
-    </tr>
-
-    <tr>
-        <td colspan="6"><b>Good Agricultural Practises:</b>'. ($assessment->agricultural_practices).'</td>
-
-
-    </tr>
-    <tr>
-        <td colspan="2"><b>Area Staff Comment:</b>'. ($assessment->area_staff_comment).'</td>
-        <td colspan="4">____________________________________________</td>
-
-    </tr>
-    <tr>
-        <td colspan="3">____________________________________________________</td>
-        <td>Sign</td>
-        <td>Date</td>
-    </tr>
-    <tr>
-        <td colspan="2"><b>ALM Comment:</b>____________________________________________</td>
-        <td colspan="4">____________________________________________________</td>
-
-    </tr>
-    <tr>
-        <td colspan="3">____________________________________________________________
-        <td>Sign</td>_____________________
-        <td>Date</td>______________________</td>
-
-
-    </tr>
-    <tr>
-        <td colspan="2"><b>Farmer\'s Signature:</b>_________________________</td>
-        <td>______________________________________</td>
-    </tr>
 </table>';
+        // <editor-fold defaultstate="collapsed" desc=" First Table ">
 
-        $the_imgs = '<div class="container" style="margin:20px;">
-                        <span style="float:left;"><img src="img/cow.jpg" style="align:left;"></span>
-                        <span></span>
-                        <span style="float: right;"><img src="img/cow.jpg" style="align:right;"></span>
-                          <span></span>
+
+
+        // </editor-fold>
+        $the_firsttable = '<table class="ze_head" border=1 cellspacing=0 cellpadding="5" align="center">
+                            <tr style="background-color:#DFDFDF;">
+                                <th colspan="4">B. INSPECTION DETAILS</th>
+                            </tr>
+                            <tr>
+                                <th width="250px">Cause of loss:</th><td>' . $assessment->cause_of_loss_id . '</td>
+                                <th width="250px">Inspection Number:</th><td></td>
+                            </tr>
+                            <tr>
+                                <th>Size of Land:</th><td>' . ($assessment-> farm->size_with_tobacco) . ' Hectares</td>
+                                <th>Inspection Date:</th><td>'. date('d-M-Y',strtotime($assessment->inspection_date)).'</td>
+                            </tr>
+                          
+                        </table>';
+        // </editor-fold>
+        $the_secondtable = '<table class="ze_head" border=1 cellspacing=0 cellpadding="5" align="center">
+                            <tr style="background-color:#DFDFDF;">
+                                <th colspan="4">C. LOSS ASSESSMENT DETAILS</th>
+                            </tr>
+                            <tr>
+                                <th width="250px">Farm Name:</th><td colspan="3">' . $assessment->farm_name . '</td>
+                                
+                            </tr>
+                            <tr style="background-color:#DFDFDF;">
+                                <th colspan="4">Leaf Damage(Damage on the surface area of the leaf quantified as % or given in cm<sup>2</sup>
+            in case of drought</th>
+                            </tr>
+                            <tr>
+                                <th>Average useful surface area <b>(a)</b></th><td colspan="3">'. ($assessment-> average_useful_sa).'</td>
+                            
+                            </tr>
+                             <tr>
+                               
+                                <th>Normal leaf surface area <b>(b)</b></th><td colspan="3">'. ($assessment-> normal_leaf_sa).'</td>
+                            </tr>
+                              <tr style="background-color:#DFDFDF;">
+                                <th colspan="4">Plant Damage(number of leaves damaged on the plant)</b></th>
+                            </tr>
+                            <tr>
+                                <th>Average no. of leaves on the plant <b>(c)</b></th><td colspan="3">'. ($assessment->average_leaves_no).'</td>
+                            
+                            </tr>
+                             <tr>
+                               
+                                <th>Normal no. leaves on the plant <b>(d)</b></th><td colspan="3">'. ($assessment-> normal_leaves_no).'</td>
+                            </tr>
+                            </tr>
+                              <tr style="background-color:#DFDFDF;">
+                                <th colspan="4">Land damage (number of plants damaged on the land parcel)</th>
+                            </tr>
+                            <tr>
+                                <th>Average no. of plants on the land <b>(e)</b></th><td colspan="3">'. ($assessment->average_plant_no).'</td>
+                            
+                            </tr>
+                             <tr>
+                               
+                                <th>Established no. of plants <b>(f)</b></th><td colspan="3">'. ($assessment-> established_plant_no).'</td>
+                            </tr>
+                            <tr>
+                                <th>Percentage Loss (%)</th><td colspan="3">'. ($assessment-> percentage_loss).'</td>
+                            
+                            </tr>
+                            <tr>
+                                <th>Leaf stage of the plant</th><td colspan="3">'. ($assessment-> leaf_stage).'</td>
+                            
+                            </tr>
+                            
+                          
+                        </table>
+                         <br />
+                                <div style="margin:10px auto;clear:both;">
+                                    <span style="margin:auto auto;width:95%; display: block;"><strong>Good Agricultural Practices</strong> _______________________________________________________________________________________</span>
+                                    <br />
+                                    <span style="margin:auto auto;width:95%; display: block;"><strong>Area Staff Comment:</strong> ______________________________________________________________________________________ <br> Signature: ____________________ Date: ________________</span>
+                                    <br />
+                                    <br />
+                                    <span style="margin:auto auto;width:95%; display: block;"><strong>UAP Crop Inspector\'s </strong>______________________________________________________________________________________ <br> Signature: ____________________ Date: ________________</span>
+                                    <br />
+                                    <br />
+                                    <span style="margin:auto auto;width:95%; display: block;"><strong>ALM Comment:</strong>______________________________________________________________________________________ <br> Signature: ____________________ Date: ________________</span>
+                                    <br />
+                                    <span style="margin:auto auto;width:95%; display: block;"><strong>Farmer\'s Signature:</strong>_________________________________________________</span>
+                                </div>
+                                <br />
+                            </div>\'';
+
+
+        $the_imgs = '<div class="image123" style="display: inline-block">
+                        <div style="float: left">
+                            <img src="img/cow.jpg" />
+                            <p>This is a photo of the Farmers land</p>
+                            
+                        </div>
+                        <div style="float: right">
+                            <img style="float: right" class="middle-img" src="img/cow.jpg" />                          
+                 
+                        </div>
+                       
                     </div>';
 
         $the_footer = '<table class="my_footer"><tr><td></td></tr></table>';
-        $the_html =$the_style . '<div class="bordered">' . $the_title . $the_header .$the_imgs. $the_footer .  '</div>';
+        $the_html =$the_style . '<div class="bordered">' . $the_title . $the_header .$the_firsttable.$the_secondtable.$the_imgs. $the_footer .  '</div>';
         return $the_html;
     }
 
