d437fafb8224b9be8d2ca74e17a58c95113de14f
mtoto.mgonjwa <mtoto.mgonjwa@gmail.com>
2016-12-05 15:52:57 +0300

    Completed
      Updated Git Ignore File
      Pulled From Uap-dev2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,13 @@
 |
 */
 
-Route::get('/auth/reset/old',function (){return view('auth.reset.old');});
+Route::get('/auth/reset/old', function () {
+    return view('auth.reset.old');
+});
 Route::post('/password/reset/old', 'UserController@storeNewPassword');
-Route::get('/', function () {   return view('auth.login'); })->name('auth.login');
+Route::get('/', function () {
+    return view('auth.login');
+})->name('auth.login');
 Route::auth();
 
 Route::group(['middleware' => ['auth']], function () {
@@ -22,48 +26,47 @@ Route::group(['middleware' => ['auth']], function () {
     Route::group(['middleware' => ['oldpassword']], function () {
 
 
+        Route::get('/dashboard', ['uses' => 'DataController@getDashboard'])->name('dashboard');
+        Route::get('/fetch/loss/causes', ['uses' => 'DataController@getLossCauses'])->name('fetch.loss.causes');
+        Route::get('/fetch/zones', ['uses' => 'DataController@getZones'])->name('fetch.zones');
+        Route::get('/fetch/seasons', ['uses' => 'DataController@getSeasons'])->name('fetch.seasons');
+        Route::get('/fetch/roles', ['uses' => 'DataController@getRoles'])->name('fetch.roles');
+        Route::get('/fetch/users', ['uses' => 'DataController@getUsers'])->name('fetch.users');
+        Route::get('/fetch/locations', ['uses' => 'DataController@getLocations'])->name('fetch.locations');
+        Route::get('/fetch/chart/data', ['uses' => 'DataController@getChartDetails'])->name('chart.data');
 
-Route::get('/dashboard',['uses' => 'DataController@getDashboard'])->name('dashboard');
-Route::get('/fetch/loss/causes',['uses' => 'DataController@getLossCauses'])->name('fetch.loss.causes');
-Route::get('/fetch/zones',['uses' => 'DataController@getZones'])->name('fetch.zones');
-Route::get('/fetch/seasons',['uses' => 'DataController@getSeasons'])->name('fetch.seasons');
-Route::get('/fetch/roles',['uses' => 'DataController@getRoles'])->name('fetch.roles');
-Route::get('/fetch/users',['uses' => 'DataController@getUsers'])->name('fetch.users');
-Route::get('/fetch/locations',['uses' => 'DataController@getLocations'])->name('fetch.locations');
-Route::get('/fetch/chart/data',['uses' => 'DataController@getChartDetails'])->name('chart.data');
 
-
-/*Report Data*/
-Route::post('/post/report/data',['uses' => 'DataController@postReportData'])->name('report.data');
+        /*Report Data*/
+        Route::post('/post/report/data', ['uses' => 'DataController@postReportData'])->name('report.data');
 
 //Get Streets
-Route::get('/getRoles', ['uses' => 'DataController@getRoles', 'as' => 'check.role']);
+        Route::get('/getRoles', ['uses' => 'DataController@getRoles', 'as' => 'check.role']);
 
-/* View Crop Inspectors*/
-Route::get('/inspectors',['uses'=> 'InspectorController@viewInspectors','as'=>'inspectors','middleware' => 'oldpassword']);
-Route::get('/fetch/inspectors',['uses'=> 'InspectorController@getCropInspectors','as'=>'fetch.inspectors']);
-Route::get('/inspector/details/{id}', ['as' => 'view_inspector_details', 'uses' => 'InspectorController@viewInspectorDetails']);
-/* Create Crop Inspectors*/
-Route::get('/inspector/create', ['as' => 'inspector.create', 'uses' => 'InspectorController@createInspectors']);
-Route::post('/inspector/create', ['as' => 'inspector.add', 'uses' => 'InspectorController@addInspector']);
-/* Update Crop Inspectors*/
-Route::post('/inspector/individual/details/update', ['as' => 'inspector.update', 'uses' => 'InspectorController@UpdateInspectorDetails']);
-/* Session Flags Crop Inspectors*/
-Route::get('/inspector/individual/edit/{id}', ['as' => 'employees.individual.details.edit', 'uses' => 'InspectorController@editIndividualInspectorDetails']);
-Route::get('/inspector/individual/edit/cancel/{id}', ['as' => 'employees.individual.details.edit.cancel', 'uses' => 'InspectorController@cancelEditIndividualInspectorDetails']);
+        /* View Crop Inspectors*/
+        Route::get('/inspectors', ['uses' => 'InspectorController@viewInspectors', 'as' => 'inspectors', 'middleware' => 'oldpassword']);
+        Route::get('/fetch/inspectors', ['uses' => 'InspectorController@getCropInspectors', 'as' => 'fetch.inspectors']);
+        Route::get('/inspector/details/{id}', ['as' => 'view_inspector_details', 'uses' => 'InspectorController@viewInspectorDetails']);
+        /* Create Crop Inspectors*/
+        Route::get('/inspector/create', ['as' => 'inspector.create', 'uses' => 'InspectorController@createInspectors']);
+        Route::post('/inspector/create', ['as' => 'inspector.add', 'uses' => 'InspectorController@addInspector']);
+        /* Update Crop Inspectors*/
+        Route::post('/inspector/individual/details/update', ['as' => 'inspector.update', 'uses' => 'InspectorController@UpdateInspectorDetails']);
+        /* Session Flags Crop Inspectors*/
+        Route::get('/inspector/individual/edit/{id}', ['as' => 'employees.individual.details.edit', 'uses' => 'InspectorController@editIndividualInspectorDetails']);
+        Route::get('/inspector/individual/edit/cancel/{id}', ['as' => 'employees.individual.details.edit.cancel', 'uses' => 'InspectorController@cancelEditIndividualInspectorDetails']);
 
 
-Route::get('/farmers' ,[ 'uses' => 'FarmersController@viewFarmers' , 'as' => 'farmers']);
+        Route::get('/farmers', ['uses' => 'FarmersController@viewFarmers', 'as' => 'farmers']);
 
-Route::get('/fetch/farmers' ,[ 'uses' => 'FarmersController@getFarmers' , 'as' => 'fetch_farmers']);
+        Route::get('/fetch/farmers', ['uses' => 'FarmersController@getFarmers', 'as' => 'fetch_farmers']);
 
-Route::get('/loss/calculation','LossCalculationController@viewLossCalculations')->name('loss.calculation');
+        Route::get('/loss/calculation', 'LossCalculationController@viewLossCalculations')->name('loss.calculation');
 
-Route::get('/loss/report','LossCalculationController@viewLossReports')->name('loss.report');
+        Route::get('/loss/report', 'LossCalculationController@viewLossReports')->name('loss.report');
 
-Route::get('fetch/loss/calculation','LossCalculationController@getLossCalculations')->name('fetch_loss_calculation');
+        Route::get('fetch/loss/calculation', 'LossCalculationController@getLossCalculations')->name('fetch_loss_calculation');
 
-Route::get('/download/loss/assessment/{assessment_id}','LossCalculationController@downloadLossAssessment')->name('assessnote.download');
+        Route::get('/download/loss/assessment/{assessment_id}', 'LossCalculationController@downloadLossAssessment')->name('assessnote.download');
 
     });
 
