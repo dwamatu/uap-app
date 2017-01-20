e2170ca89dddf4c524868ee09fe7583bc36d7e06
mtoto.mgonjwa <mtoto.mgonjwa@gmail.com>
2017-01-20 17:17:42 +0300

    Updated Views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,10 +1,11 @@
 @extends('auth.master')
 @section('auth-content')
+    <div class="row">
+        <div class="col-sm-6">
 
-    <div class="panel-heading">
-        <h3 class="panel-title text-center">Reset Password</h3>
-    </div>
-    <div class="panel-body">
+            <p class="login-box-msg">Reset Password</p>
+
+
         @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
@@ -14,29 +15,38 @@
         <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
             {{ csrf_field() }}
 
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
 
-                <div class="col-md-12">
-                    <input id="email" type="email" class="form-control" name="email"
-                           value="{{ old('email') }}" placeholder="E-Mail Address">
 
-                    @if ($errors->has('email'))
-                        <span class="help-block">
+                <input id="email" type="email" class="form-control" name="email"
+                       value="{{ old('email') }}" placeholder="E-Mail Address">
+
+                @if ($errors->has('email'))
+                    <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                    @endif
-                </div>
+                @endif
+
             </div>
 
             <div class="form-group">
-                <div class="col-md-12 ">
-                    <button type="submit"
-                            class="btn-block no-margin large btn-primary gradient-light-linear color green button">
-                        <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
-                    </button>
-                </div>
+
+                <button type="submit"
+                        class="btn-block no-margin large btn-primary gradient-light-linear color green button">
+                    <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
+                </button>
             </div>
         </form>
+        </div>
+        <div class="col-sm-6">
+            <div class="logo" style="padding: 40px 25px 10px ">
+                <span class="logosu"><a href="#"><img src="{{URL::asset('img/uap/uap.png')}}" alt="Uap logo"></a></span>
+                <span class="logoso"><a href="#"><img src="{{URL::asset('img/uap/old.png')}}" alt="Old mutual logo"></a></span>
+            </div>
+        </div>
+
     </div>
+
+
 
 @endsection
