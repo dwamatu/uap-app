c3d8dbc2c8795a4663fc762043419482d71825b0
mtoto.mgonjwa <mtoto.mgonjwa@gmail.com>
2017-01-24 14:35:50 +0300

    Updated User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,34 +11,7 @@ use DB;
 
 class UserController extends Controller
 {
-//    public function postSignIn(Request $request)
-//    {
-//        $this->validate($request, [
-//            'email'=> 'required',
-//            'password'=>'required'
-//        ]);
-//
-//
-//        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
-//            $userId = Auth::user()->id;
-//            $active = DB::table('users')->where('id', $userId)->value('password_updated_at');
-//            $dt=(Carbon::createFromFormat('Y-m-d H:i:s',$active));
-//            $now =Carbon::now()->subMonths(3);
-//
-////            dd($now ->gt($dt));
-//
-//            if ($now ->gt($dt) != true )
-//                return redirect()->route('dashboard');
-//            else {
-//
-//                return redirect('/auth/reset/old');
-//            }
-//
-//
-//        }
-//
-//        return redirect()-> back();
-//    }
+
     public function storeNewPassword(Request $request)
     {
         $this->validate($request, [
